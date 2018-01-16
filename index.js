8a0481b5a5392b305328d6e1e51b5993fcdbbb21
Ryan Spargo <ryanspargo@gmail.com>
2018-01-16 05:31:02 -0700

    Major change commit. Added basic wireframing.

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-var express = require('express'),
-pug = require('pug'),
-path = require('path'),
+var express = require('express');
 var app = express();
+pug = require('pug');
+path = require('path');
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname + '/public')));
-app.use(express.static(path.join(__dirname + '/public/images')));
+// app.use(express.static(path.join(__dirname + '/public/images')));
 
 app.get('/', function(req, res) {
     res.render('home');
+});
+app.get('/gallery', function(req, res) {
+    res.render('gallery');
+});
+app.get('/shiny', function(req, res) {
+    res.render('shiny');
+});
+app.get('/isaac', function(req, res) {
+    res.render('isaac');
+});
+app.get('/vseeks', function(req, res) {
+    res.render('vseeks');
 });
 
 app.listen(3000);
