ba0895bfe97284df0aa38da297190c5000f4b1c5
Ryan Spargo <ryanspargo@gmail.com>
2018-02-27 06:41:13 -0700

    Updated the WIP landing page.

--- index.js
+++ index.js
@@ -6,22 +6,31 @@ path = require('path');
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname + '/public')));
-// app.use(express.static(path.join(__dirname + '/public/images')));
 
 app.get('/', function(req, res) {
     res.render('home');
 });
-app.get('/gallery', function(req, res) {
-    res.render('gallery');
+app.get('/:page', function(req, res) {
+    res.render(req.params.page);
 });
-app.get('/shiny', function(req, res) {
-    res.render('shiny');
-});
-app.get('/isaac', function(req, res) {
-    res.render('isaac');
-});
-app.get('/vseeks', function(req, res) {
-    res.render('vseeks');
-});
+
+// app.get('/', function(req, res) {
+//     res.render('home');
+// });
+// app.get('/gallery', function(req, res) {
+//     res.render('gallery');
+// });
+// app.get('/shiny', function(req, res) {
+//     res.render('shiny');
+// });
+// app.get('/isaac', function(req, res) {
+//     res.render('isaac');
+// });
+// app.get('/vseeks', function(req, res) {
+//     res.render('vseeks');
+// });
+// app.get('/framework', function(req, res) {
+//     res.render('framework');
+// });
 
 app.listen(process.env.PORT||3000);
